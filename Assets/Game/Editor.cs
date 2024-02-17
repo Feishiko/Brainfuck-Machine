cae3bc0227f0611d7e444930e94c6c4632cf3311
Feishiko <690206178@qq.com>
2024-02-18 00:28:09 +0800

    Brainfuck Start

--- Assets/Game/Editor.cs
+++ Assets/Game/Editor.cs
@@ -20,9 +20,11 @@ public class Editor : MonoBehaviour
     private int pointerPosition = 0;
     private int loopIter = 0;
     private string commands;
+    private int commandPosition = 0;
     private bool isActive = false;
     private bool isRunning = false;
     private int testPassed = 0; // Should be 100
+    private int currentTest = 0; // Maybe only show on text
     public ILevel level;
     void Start()
     {
@@ -40,24 +42,24 @@ public class Editor : MonoBehaviour
             // MemoryCell
             cells[iter] = Instantiate<Cell>(cell, this.transform);
             var pos = cells[iter].GetComponent<RectTransform>().position;
-            cells[iter].GetComponent<RectTransform>().position = new Vector3(pos.x + iter * 80, pos.y, 0);
+            cells[iter].GetComponent<RectTransform>().position = new Vector3(pos.x + iter * 160, pos.y, 0);
 
             // InputCell
             inputCells[iter] = Instantiate<InputCell>(inputCell, this.transform);
             var inputPos = inputCells[iter].GetComponent<RectTransform>().position;
-            inputCells[iter].GetComponent<RectTransform>().position = new Vector3(inputPos.x + iter * 80, inputPos.y, 0);
+            inputCells[iter].GetComponent<RectTransform>().position = new Vector3(inputPos.x + iter * 160, inputPos.y, 0);
             inputCells[iter].GetComponent<Text>().text = level.inputs[iter, 0].ToString();
 
             // OutputCell
             outputCells[iter] = Instantiate<OutputCell>(outputCell, this.transform);
             var outputPos = outputCells[iter].GetComponent<RectTransform>().position;
-            outputCells[iter].GetComponent<RectTransform>().position = new Vector3(outputPos.x + iter * 80, outputPos.y, 0);
+            outputCells[iter].GetComponent<RectTransform>().position = new Vector3(outputPos.x + iter * 160, outputPos.y, 0);
             outputCells[iter].GetComponent<Text>().text = "-1";
 
             // TargetCell
             targetCells[iter] = Instantiate<TargetCell>(targetCell, this.transform);
             var targetPos = targetCells[iter].GetComponent<RectTransform>().position;
-            targetCells[iter].GetComponent<RectTransform>().position = new Vector3(targetPos.x + iter * 80, targetPos.y, 0);
+            targetCells[iter].GetComponent<RectTransform>().position = new Vector3(targetPos.x + iter * 160, targetPos.y, 0);
             targetCells[iter].GetComponent<Text>().text = level.outputs[iter, 0].ToString();
         }
     }
@@ -70,11 +72,102 @@ public class Editor : MonoBehaviour
             cells[iter].GetComponent<Text>().color = Color.white;
         }
         cells[pointerPosition].GetComponent<Text>().color = Color.yellow;
+
+        editor.interactable = !isRunning;
+
+        if (isActive)
+        {
+            ProgramRun();
+        }
+    }
+
+    public void PreviewNext()
+    {
+        if (!isRunning)
+        {
+            if (currentTest < 99)
+            {
+                currentTest += 1;
+            }
+            for (var iter = 0; iter < 6; iter++)
+            {
+                inputCells[iter].GetComponent<Text>().text = level.inputs[iter, currentTest].ToString();
+                targetCells[iter].GetComponent<Text>().text = level.outputs[iter, currentTest].ToString();
+            }
+        }
+        GameObject.Find("TestPercent").GetComponent<Text>().text = $"{currentTest + 1}/100";
+    }
+
+    public void PreviewPrevious()
+    {
+        if (!isRunning)
+        {
+            if (currentTest > 0)
+            {
+                currentTest -= 1;
+            }
+            for (var iter = 0; iter < 6; iter++)
+            {
+                inputCells[iter].GetComponent<Text>().text = level.inputs[iter, currentTest].ToString();
+                targetCells[iter].GetComponent<Text>().text = level.outputs[iter, currentTest].ToString();
+            }
+        }
+        GameObject.Find("TestPercent").GetComponent<Text>().text = $"{currentTest + 1}/100";
     }
 
     public void BrainfuckStep()
     {
+        if (!isRunning)
+        {
+            ProgramLoad();
+            currentTest = 0;
+            for (var iter = 0; iter < 6; iter++)
+            {
+                inputCells[iter].GetComponent<Text>().text = level.inputs[iter, currentTest].ToString();
+                targetCells[iter].GetComponent<Text>().text = level.outputs[iter, currentTest].ToString();
+            }
+            GameObject.Find("TestPercent").GetComponent<Text>().text = $"{currentTest + 1}/100";
+        }
         isRunning = true;
+        isActive = false;
+        ProgramRun();
+    }
+
+    public void BrainfuckRun()
+    {
+        if (!isRunning)
+        {
+            ProgramLoad();
+            currentTest = 0;
+            for (var iter = 0; iter < 6; iter++)
+            {
+                inputCells[iter].GetComponent<Text>().text = level.inputs[iter, currentTest].ToString();
+                targetCells[iter].GetComponent<Text>().text = level.outputs[iter, currentTest].ToString();
+            }
+            GameObject.Find("TestPercent").GetComponent<Text>().text = $"{currentTest + 1}/100";
+        }
+        isRunning = true;
+        isActive = true;
+    }
+
+    public void ProgramLoad()
+    {
+        commands = "";
+        loopIter = 0;
+        var commandsArray = editor.text.ToCharArray();
+        foreach (var item in commandsArray)
+        {
+            if (item == '+' || item == '-' || item == '<' || item == '>' || item == '[' || item == ']' || item == ',' || item == '.')
+            {
+                commands += item;
+            }
+        }
+    }
+
+    public void ProgramRun()
+    {
+        Debug.Log(commands.Substring(commandPosition, 1));
+        commandPosition += 1;
     }
 
     public void BrainfuckStop()
